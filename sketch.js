e2e6b73a6784c50d3b87e76fdaa257c0c78e95b4
argusto <145466903+AugustoByczkovski@users.noreply.github.com>
2023-10-17 10:45:29 -0300

    mudando tudo

--- sketch.js
+++ sketch.js
@@ -1,15 +1,33 @@
-function setup() {
-  createCanvas(1100, 800);
-  background("white")
-}
+let corAtual;
+let tamanhoBrush;
 
-function draw() {
-  stroke("black");
-  fill("black");
-  
-  // console.log(mouseIsPressed)
-  
-  if (mouseIsPressed) {
-    rect(mouseX, mouseY, 20, 35);
+function setup() { 
+  createCanvas(1100, 800); 
+  background("white");
+  corAtual = "black"; // cor inicial
+  tamanhoBrush = 20; // tamanho inicial do brush
+} 
+
+function draw() { 
+  stroke(corAtual); 
+  fill(corAtual); 
+
+  if (mouseIsPressed) { 
+    rect(mouseX, mouseY, tamanhoBrush, tamanhoBrush); 
+  } 
+} 
+
+function keyPressed() {
+  if (key === 'b') {
+    corAtual = "white";
+  } else if (key === 'p') {
+    corAtual = "black";
+  } else if (keyCode === UP_ARROW) {
+    tamanhoBrush += 5;
+  } else if (keyCode === DOWN_ARROW) {
+    tamanhoBrush -= 5;
+    if (tamanhoBrush < 5) {
+      tamanhoBrush = 5; // não deixa o tamanho do brush ser menor que 5
+    }
   }
 }
